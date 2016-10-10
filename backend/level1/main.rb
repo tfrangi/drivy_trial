3482fdc32d4307276bddcc94658ac9250caea55e
tfrangi <tristan@frangi.fr>
2016-10-10 13:25:30 +0200

    Level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,45 @@
 require "json"
+require "awesome_print"
+require 'Date'
 
 # your code
+def data 
+	file = File.read('data.json')
+	data = JSON.parse(file)
+end
+
+def cars
+	data['cars']
+end
+
+def rental_price rental
+	car = cars[(rental['car_id']-1)]
+	t_price = time_price car, rental['start_date'], rental['end_date']
+	d_price = distance_price car, rental['distance']
+
+	t_price + d_price
+end
+
+def time_price car, start_date, end_date
+	nb_days = (Date.parse(end_date) - Date.parse(start_date)).to_i
+	car['price_per_day'] * (nb_days + 1)
+end
+
+def distance_price car, distance
+	car['price_per_km'] * distance
+end
+
+
+
+##############
+
+rentals = data['rentals']
+
+output = {rentals: []}
+
+rentals.each do |rental|
+	output[:rentals] << {id: rental['id'], price: rental_price(rental)}
+end
+
+ap output
+File.open("sample.json", "wb") { |file| file.puts JSON.pretty_generate(output) }
